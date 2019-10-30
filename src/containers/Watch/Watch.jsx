01cdaf669e635c628404503622854429fbe37e08
Christopher Iliyas <christopheriliyas@gmail.com>
2019-10-30 20:56:47 +0100

    create related videos component

--- src/containers/Watch/Watch.jsx
+++ src/containers/Watch/Watch.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Watch.scss';
-import Video from '../../components/Video/Video';
+import VideoPreview from '../../components/VideoPreview/VideoPreview';
 
 const Watch = () => {
     return (
-        <div style={{maxWidth: '80%'}}>
-            <Video id='euSAgaub3m8'/>
-        </div>
+        <>
+            <VideoPreview horizontal={true} />
+            <VideoPreview />
+        </>
     )
 }
 
