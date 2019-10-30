3c28bd2b85cb99544d7dd8118a22c2e1c96bdf3f
Christopher Iliyas <christopheriliyas@gmail.com>
2019-10-30 13:47:19 +0100

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home/Home';
+import Watch from './containers/Watch/Watch';
 import AppLayout from './components/AppLayout/AppLayout';
 
 const App = () => {
   return (
       <AppLayout>
-        <Home />
+        <Switch>
+          <Route path="/watch" component={Watch} />
+          <Route path="/" component={Home} />
+        </Switch>
       </AppLayout>
   );
 };
---
+++ src/containers/Watch/Watch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Watch.scss';
+
+const Watch = () => {
+    return (
+        <div>
+                Test Watch
+        </div>
+    )
+}
+
+export default Watch;
