adef422ff1e5b57c0d79058c25f67d330ae4ba42
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-06 15:45:06 +0100

    add rating component

--- src/containers/Watch/Watch.jsx
+++ src/containers/Watch/Watch.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './Watch.scss';
 import Video from '../../components/Video/Video';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
+import VideoMetaData from '../../components/VideoMetaData/VideoMetaData';
 
 const Watch = () => {
     return (
         <div className='watch-grid'>
             <Video className='video' id='-7fuHEEmEjs' />
-            <div className='metadata' style={{width: '100%', height: '100px', background: '#F91112'}}>Metadata</div>
+            <VideoMetaData className='metadata' viewCount={1000}/>
             <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
             <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
             <RelatedVideos className='relatedVideos'/>
