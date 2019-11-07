d0effa693a60ba3d96148ddb9317a8ec89475ca5
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-07 22:29:00 +0100

    setup redux and store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from './store/configureStore';
 
-ReactDOM.render(<BrowserRouter>
+const store = configureStore();
+
+ReactDOM.render(
+            <Provider store={store}>
+                <BrowserRouter>
                     <App />
-                   </BrowserRouter>, document.getElementById('root'));
+                </BrowserRouter>
+            </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
