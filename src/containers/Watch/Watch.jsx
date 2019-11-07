d2e70c4b595ee28582a4dc9e37c5e1c12dd52342
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-07 21:36:35 +0100

    create comments component

--- src/containers/Watch/Watch.jsx
+++ src/containers/Watch/Watch.jsx
@@ -4,6 +4,7 @@ import Video from '../../components/Video/Video';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 import VideoMetaData from '../../components/VideoMetaData/VideoMetaData';
 import VideoInfoBox from '../../components/VideoInfoBox/VideoInfoBox';
+import Comments from '../Comments/Comments';
 
 const Watch = () => {
     return (
@@ -11,7 +12,7 @@ const Watch = () => {
             <Video className='video' id='-7fuHEEmEjs' />
             <VideoMetaData className='metadata' viewCount={1000}/>
             <VideoInfoBox className='video-info-box'/>
-            <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
+            <Comments className='comments'/>
             <RelatedVideos className='relatedVideos'/>
       </div>
     );
