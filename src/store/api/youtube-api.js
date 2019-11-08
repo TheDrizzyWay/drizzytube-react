dae78a634c38c31cc8e1bb0af71300a6e7da5a45
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-08 10:14:45 +0100

    setup popular videos saga

--- src/store/api/youtube-api.js
+++ src/store/api/youtube-api.js
@@ -1,4 +1,4 @@
-export default function buildApiRequest(requestMethod, path, params, properties) {
+export function buildApiRequest(requestMethod, path, params, properties) {
     params = removeEmptyParams(params);
     let request;
     if (properties) {
@@ -57,4 +57,21 @@ export default function buildApiRequest(requestMethod, path, params, properties)
     }
     return resource;
   };
+
+  export function buildMostPopularVideosRequest(amount = 12, loadDescription = false, nextPageToken) {
+    let fields = 'nextPageToken,prevPageToken,items(contentDetails/duration,id,snippet(channelId,channelTitle,localized/title,publishedAt,thumbnails/medium,title),statistics/viewCount),pageInfo(totalResults)';
+    if (loadDescription) {
+      fields += ',items/snippet/description';
+    }
+    return buildApiRequest('GET',
+      '/youtube/v3/videos',
+      {
+        part: 'snippet,statistics,contentDetails',
+        chart: 'mostPopular',
+        maxResults: amount,
+        regionCode: 'US',
+        pageToken: nextPageToken,
+        fields,
+      }, null);
+  }
   
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,17 @@
-import {all} from 'redux-saga/effects';
-export default function* (){
-  yield all([]);
+import { all, call, put, fork } from 'redux-saga/effects';
+import { watchMostPopularVideos } from './video';
+
+export function* fetchEntity(request, entity, ...args) {
+  try {
+    const response = yield call(request);
+    yield put(entity.success(response.result, ...args));
+  } catch (error) {
+    yield put(entity.failure(error, ...args));
+  }
+}
+
+export default function *rootSaga() {
+  yield all([
+    fork(watchMostPopularVideos)
+  ]);
 }
