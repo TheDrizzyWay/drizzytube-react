23def329e6c8d69bc2eff61634976b84aa9bf150
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-09 14:13:02 +0100

    setup categories reducer

--- src/store/reducers/video.js
+++ src/store/reducers/video.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { MOST_POPULAR, VIDEO_CATEGORIES } from '../actions/video';
+import { MOST_POPULAR, VIDEO_CATEGORIES, MOST_POPULAR_BY_CATEGORY } from '../actions/video';
 import { SUCCESS } from '../actions';
 
 const initialState = {
@@ -44,12 +44,57 @@ const reduceFetchMostPopularVideos = (response, prevState) => {
     };
   };
 
-const videosReducer = (state = initialState, { type, response }) => {
+  const groupVideosByIdAndCategory = (response) => {
+    const videos = response.items;
+    const byId = {};
+    const byCategory = {
+      totalResults: response.pageInfo.totalResults,
+      nextPageToken: response.nextPageToken,
+      items: [],
+    };
+  
+    videos.forEach((video) => {
+      byId[video.id] = video;
+  
+      const items = byCategory.items;
+      if(items && items) {
+        items.push(video.id);
+      } else {
+        byCategory.items = [video.id];
+      }
+    });
+  
+    return { byId, byCategory };
+  };
+
+  const reduceFetchMostPopularVideosByCategory = (responses, categories, prevState) => {
+    let videoMap = {};
+    let byCategoryMap = {};
+  
+    responses.forEach((response, index) => {
+      if (response.status === 400) return;
+  
+      const categoryId = categories[index];
+      const {byId, byCategory} = groupVideosByIdAndCategory(response.result);
+      videoMap = {...videoMap, ...byId};
+      byCategoryMap[categoryId] = byCategory;
+    });
+  
+    return {
+      ...prevState,
+      byId: {...prevState.byId, ...videoMap},
+      byCategory: {...prevState.byCategory, ...byCategoryMap},
+    };
+  };
+
+const videosReducer = (state = initialState, { type, response, categories }) => {
   switch (type) {
     case MOST_POPULAR[SUCCESS]:
       return reduceFetchMostPopularVideos(response, state);
     case VIDEO_CATEGORIES[SUCCESS]:
       return reduceFetchVideoCategories(response, state);
+    case MOST_POPULAR_BY_CATEGORY[SUCCESS]:
+      return reduceFetchMostPopularVideosByCategory(response, categories, state);
     default:
       return state;
   }
@@ -70,6 +115,20 @@ export const getVideoCategoryIds = createSelector(
   state => state.videos.categories,
   (categories) => {
     return Object.keys(categories || {});
+  }
+);
+
+export const getVideosByCategory = createSelector(
+  state => state.videos.byCategory,
+  state => state.videos.byId,
+  state => state.videos.categories,
+  (videosByCategory, videosById, categories) => {
+    return Object.keys(videosByCategory || {}).reduce((accumulator, categoryId) => {
+      const videoIds = videosByCategory[categoryId].items;
+      const categoryTitle = categories[categoryId];
+      accumulator[categoryTitle] = videoIds.map(videoId => videosById[videoId]);
+      return accumulator;
+    }, {});
   }
 );
 
